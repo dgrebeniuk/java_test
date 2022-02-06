f33d7ae33e8d22931e4457eb2625e01196b7a8ee
dmgrebenyuk <dmtrgrbnk@gmail.com>
2022-02-06 18:19:46 +0300

    добавлены новые функции

--- Sandbox/src/main/java/ru/qa/javatest/sandbox/First.java
+++ Sandbox/src/main/java/ru/qa/javatest/sandbox/First.java
@@ -3,7 +3,18 @@ package ru.qa.javatest.sandbox;
 public class First {
 
    public static void main(String[] args) {
-      System.out.println("Hello, World! I am God");
+      hello("God");
+      double x = 13;
+      double y = 1;
+      System.out.println("Площадь прямоугольника = " + x + " и " + y + " = " + area(x,y));
+   }
+
+   public static void hello(String a) {
+      System.out.println("Hello, World! I am " + a + "!");
+   }
+
+   public static Double area(Double x, Double y) {
+      return x * y;
    }
 
 }
