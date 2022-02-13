e4ba2105ac3157ecb2627f7fe3f7a82beca40364
dmgrebenyuk <dmtrgrbnk@gmail.com>
2022-02-13 15:09:01 +0300

    first tests

--- Sandbox/src/test/java/ru/qa/javatest/sandbox/SquareTests.java
+++ Sandbox/src/test/java/ru/qa/javatest/sandbox/SquareTests.java
@@ -1,11 +1,14 @@
 package ru.qa.javatest.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SquareTests {
 
-
+   @Test
    public void testArea() {
-      Square s = new Square(5);
-      assert s.area() == 25;
+      Square s = new Square(7);
+      Assert.assertEquals(s.area(),49);
 
    }
 }
